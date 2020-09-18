e2cfc2b4db9bbc537b297cfe9df7b147f5c25f33
cookiespiggy <quqixiaozhu@163.com>
2020-09-18 18:18:17 +0800

    cglib

--- map/src/main/java/cn/it/demo/MyTest.java
+++ map/src/main/java/cn/it/demo/MyTest.java
@@ -1,14 +1,32 @@
 package cn.it.demo;
 
+import net.sf.cglib.beans.BeanGenerator;
+import net.sf.cglib.beans.BeanMap;
+
+import java.lang.reflect.Method;
+
 public class MyTest {
     public static void main(String[] args) {
 
-        UserDO userDO = new UserDO().setId(1).setUsername("yudaoyuanma").setPassword("buzhidao");
+//        UserDO userDO = new UserDO().setId(1).setUsername("yudaoyuanma").setPassword("buzhidao");
+//
+//        MyBO myBO = UserConvert.INSTANCE.convertMyBO(userDO);
+//
+//        System.out.println(myBO);
 
-        MyBO myBO = UserConvert.INSTANCE.convertMyBO(userDO);
+        BeanGenerator beanGenerator = new BeanGenerator();
+        beanGenerator.addProperty("id", Integer.class);
 
-        System.out.println(myBO);
 
+        BeanMap beanMap = BeanMap.create(beanGenerator);
+//        beanMap.
 
+        beanGenerator.setSuperclass(UserBO.class);
+        Object o = beanGenerator.create();
+        Class<?> aClass = o.getClass();
+        Method[] methods = aClass.getMethods();
+        for (Method method : methods) {
+            System.out.println(method);
+        }
     }
 }
