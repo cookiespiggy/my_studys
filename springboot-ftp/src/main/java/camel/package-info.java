7561930f99b480ef8468c8b1cfbae82ea04e12f5
cookiespiggy <quqixiaozhu@163.com>
2020-09-24 11:30:15 +0800

    camel自己的牛逼定义

---
+++ springboot-ftp/src/main/java/camel/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * camel 是 from to 配置一系列process的过程
+ *
+ * from to 叫router
+ * from file to file
+ * from hdfs to file
+ * from ftp to hdfs
+ * from hdfs to es
+ * 至于本ZipFileFilter 为ftp的filter
+ * ...
+ */
+package camel;
